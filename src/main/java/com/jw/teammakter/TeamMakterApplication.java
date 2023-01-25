eb2c4e54dac55b2929d585ac3e48ce68cf090339
jongwankim <jongbell4@gmail.com>
2023-01-25 11:01:43 +0900

    first init

---
+++ src/main/java/com/jw/teammakter/TeamMakterApplication.java
@@ -0,0 +1,13 @@
+package com.jw.teammakter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamMakterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamMakterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jw/teammakter/TeamMakterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jw.teammakter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamMakterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
