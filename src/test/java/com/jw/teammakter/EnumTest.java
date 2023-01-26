450c1ed95732c657675e1c3ff944f7c593134e4b
jongwankim <jongbell4@gmail.com>
2023-01-26 14:05:24 +0900

    - Position rating + Tier rating  - 조합 결과로 4개 그룹 분리 후 팀 결정 방식 적용

--- src/test/java/com/jw/teammakter/EnumTest.java
+++ src/test/java/com/jw/teammakter/EnumTest.java
@@ -1,7 +1,13 @@
 package com.jw.teammakter;
 
 import com.jw.teammakter.domain.Position;
+import com.jw.teammakter.domain.Tier;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class EnumTest {
 
@@ -19,6 +25,23 @@ public class EnumTest {
 
     @Test
     void Enum_Iterating_test(){
-        Position.stream().forEach(System.out::println);
+        List<Integer> positionPoint = Position.stream()
+                .map(p -> p.getPoint())
+                .collect(Collectors.toList());
+        List<Integer> tierPoint = Tier.stream()
+                .map(t -> t.getPoint())
+                .collect(Collectors.toList());
+
+        ArrayList<Integer> resultArray = new ArrayList<Integer>();
+        for(int i = 0; i < positionPoint.size(); i++){
+            for(int j = 0; j < tierPoint.size(); j++){
+                resultArray.add((positionPoint.get(i) + tierPoint.get(j)));
+            }
+
+        }
+        Collections.sort(resultArray);
+
+        resultArray.stream().forEach(System.out::println);
+
     }
 }
