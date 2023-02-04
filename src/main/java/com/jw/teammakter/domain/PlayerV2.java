5dfb5d13e58052e41af2d03987ab88f2f37b8d10
jongwan <jongbell4@gmail.com>
2023-02-04 17:24:58 +0900

    proj-lol-team-maker-4 #4 팀 분배 방법
    - main / sub position 분리

---
+++ src/main/java/com/jw/teammakter/domain/PlayerV2.java
@@ -0,0 +1,46 @@
+package com.jw.teammakter.domain;
+
+public class PlayerV2 {
+
+    public PlayerV2(){};
+
+    public PlayerV2(String name, String positionMain, String positionSub, String tier){
+        this.name = name;
+        this.positionMain = Position.valueOf(positionMain);
+        this.positionSub = Position.valueOf(positionSub);
+        this.tier = Tier.valueOf(tier);
+
+    }
+
+    private int id;
+
+    private String name;
+
+    private Position positionMain;
+
+    private Position positionSub;
+
+    private Tier tier;
+
+    public int getPositionMainPoint(){
+        return this.positionMain.getPoint();
+    }
+
+    public String getPositionMain(){
+        return this.positionMain.name();
+
+    }
+
+    public int getPositionSubPoint(){
+        return this.positionSub.getPoint();
+    }
+
+    public String getPositionSub(){
+        return this.positionSub.name();
+    }
+
+
+
+
+}
+
