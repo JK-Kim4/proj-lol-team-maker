f3255ac08b19dd8eeb7b0183d482fd025d3ff035
jongwan <jongbell4@gmail.com>
2023-04-20 22:21:25 +0900

    proj-lol-team-maker-19 #16 플레이어 화면

--- src/test/java/com/jw/teammaker/PlayerServiceTest.java
+++ src/test/java/com/jw/teammaker/PlayerServiceTest.java
@@ -1,9 +1,16 @@
 package com.jw.teammaker;
 
+import com.jw.teammaker.entity.Player;
+import com.jw.teammaker.entity.enumtype.Position;
+import com.jw.teammaker.entity.enumtype.Tier;
+import com.jw.teammaker.presentation.dto.PlayerSaveDto;
 import com.jw.teammaker.service.PlayerService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
 
 @SpringBootTest
 @ActiveProfiles("dev")
@@ -11,4 +18,47 @@ public class PlayerServiceTest {
 
     @Autowired
     PlayerService playerService;
+
+    @Test
+    public void player_save_and_find_test(){
+        //given
+        PlayerSaveDto dto = new PlayerSaveDto();
+        dto.setName("player");
+        dto.setTier(Tier.TWO.name());
+        dto.setMainPosition(Position.JNG.name());
+        dto.setSubPosition(Position.MID.name());
+
+        System.out.println("dto = " + dto.toString());
+
+        //when
+        Long result = playerService.save(dto);
+
+        Player player = playerService.findById(result);
+
+
+        //then
+        System.out.println("save result = " + result);
+        System.out.println("player id = " +player.getId());
+
+
+    }
+
+    @Test
+    public void player_delete_test(){
+        //given
+        PlayerSaveDto dto = new PlayerSaveDto();
+        dto.setName("player");
+        dto.setTier(Tier.TWO.name());
+        dto.setMainPosition(Position.JNG.name());
+        dto.setSubPosition(Position.MID.name());
+
+        //when
+        Long result = playerService.save(dto);
+        playerService.delete(result);
+        List<Player> players = playerService.findAll();
+
+        //then
+        System.out.println("player size = " + players.size());
+
+    }
 }
