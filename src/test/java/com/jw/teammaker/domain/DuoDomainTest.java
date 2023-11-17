ac31717253b547dbb73873b435ede5fd428f377b
jongwan <jw.kim@changbi.com>
2023-11-17 17:57:09 +0900

    feat: 듀오 / 플레이어 테스트 코드

--- src/test/java/com/jw/teammaker/domain/DuoDomainTest.java
+++ src/test/java/com/jw/teammaker/domain/DuoDomainTest.java
@@ -1,8 +1,25 @@
 package com.jw.teammaker.domain;
 
+import org.junit.jupiter.api.Test;
+
 /**
  * Dou Domain Unit Test
  *
  * */
 public class DuoDomainTest {
+
+    @Test
+    public void 듀오플레이어_추가_테스트(){
+
+    }
+
+    @Test
+    public void 듀오플레이어_총점_평가점수_테스트(){
+
+    }
+
+    @Test
+    public void 듀오플레이어_평균_평가점수_테스트(){
+
+    }
 }
